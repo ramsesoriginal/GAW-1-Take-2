fc7a9229d82b35dc83ed4cd12cd1ca9794f52af8
Stefan Insam <ramsesoriginal@gmail.com>
2014-10-28 04:00:27 +0100

    Game about bouncing a ball? Hm. Not sure

---
+++ Assets/Gameplay/Scirpts/ControllBall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllBall : MonoBehaviour {
+
+	public float speed;
+	public float jumpstrength;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var h = Input.GetAxis("Horizontal");
+		var v = Input.GetAxis("Vertical");
+		rigidbody.AddForce (Vector3.right * h * speed + Vector3.up * v * jumpstrength);
+	}
+}
---
+++ Assets/Gameplay/Scirpts/RotateCrate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCrate : MonoBehaviour {
+
+	public float rotateSpeed = 5;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed, Space.World);
+	}
+}
