cde0ada355213860e7643ccb51caa08412d34994
Stefan Insam <ramsesoriginal@gmail.com>
2014-10-29 17:05:37 +0100

    Fixed shitty camera movement bug!

--- Assets/Gameplay/Scripts/MoveCameraHere.cs
+++ Assets/Gameplay/Scripts/MoveCameraHere.cs
@@ -6,21 +6,32 @@ public class MoveCameraHere : MonoBehaviour {
 	public Transform target;
 	public float speed = 0.1f;
 
+	private static float distance;
+	private static Vector3 p;
+	private static Quaternion r;
+	private static Transform _target;
+	private static float _laststartTime;
+	private float laststartTime;
+
 	void OnTriggerEnter () {
+		_target = target;
+		laststartTime = _laststartTime = Time.time;
 		StartCoroutine("Move");
 	}
 
 	IEnumerator Move() {
-		var distance = (transform.position - target.position).magnitude;
-		var p = GameObject.FindGameObjectWithTag ("MainCamera").transform.position;
-		var r = GameObject.FindGameObjectWithTag ("MainCamera").transform.rotation;
-		for (float f = 0; f < distance; f += 0.1f) {
-			GameObject.FindGameObjectWithTag ("MainCamera").transform.position = Vector3.Lerp(p,target.position,f/distance);
-			GameObject.FindGameObjectWithTag ("MainCamera").transform.rotation = Quaternion.Lerp(r,target.rotation,f/distance); 
+		distance = (transform.position - _target.position).magnitude;
+		p = GameObject.FindGameObjectWithTag ("MainCamera").transform.position;
+		r = GameObject.FindGameObjectWithTag ("MainCamera").transform.rotation;
+		for (float f = 0; f < distance && laststartTime == _laststartTime; f += speed) {
+			GameObject.FindGameObjectWithTag ("MainCamera").transform.position = Vector3.Lerp(p,_target.position,f/distance);
+			GameObject.FindGameObjectWithTag ("MainCamera").transform.rotation = Quaternion.Lerp(r,_target.rotation,f/distance); 
 			yield return null;
 		}
-		GameObject.FindGameObjectWithTag ("MainCamera").transform.position = target.position;
-		GameObject.FindGameObjectWithTag ("MainCamera").transform.rotation = target.rotation;
+		if (laststartTime == _laststartTime) {
+			GameObject.FindGameObjectWithTag ("MainCamera").transform.position = _target.position;
+			GameObject.FindGameObjectWithTag ("MainCamera").transform.rotation = _target.rotation;
+		}
 	}
 
 }
