21b1165554936cf59a42ede96e4752619cbcd04d
Stefan Insam <ramsesoriginal@gmail.com>
2014-10-30 19:06:04 +0100

    Working on lvl 4, added trail

---
+++ Assets/Gameplay/Scripts/RotatePropeller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePropeller : MonoBehaviour {
+
+	
+	public float rotateSpeed = 5;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed, Space.World);
+	}
+}
---
+++ Assets/Gameplay/Scripts/cameraShake.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+public class cameraShake : MonoBehaviour
+{
+	private Vector3 originPosition;
+	private Quaternion originRotation;
+	public float shake_decay_start;
+	public float shake_intensity_start;
+	private float shake_decay;
+	private float shake_intensity;
+
+	
+	void Update (){
+		if (shake_intensity > 0.001f){
+			Camera.main.transform.position = originPosition + Random.insideUnitSphere * shake_intensity;
+			Camera.main.transform.rotation = new Quaternion(
+				originRotation.x + Random.Range (-shake_intensity,shake_intensity) * .2f,
+				originRotation.y + Random.Range (-shake_intensity,shake_intensity) * .2f,
+				originRotation.z + Random.Range (-shake_intensity,shake_intensity) * .2f,
+				originRotation.w + Random.Range (-shake_intensity,shake_intensity) * .2f);
+			shake_intensity -= shake_decay;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Crate") {
+			Shake ();
+		}
+	}
+	
+	public void Shake(){
+		originPosition = Camera.main.transform.position;
+		originRotation = Camera.main.transform.rotation;
+		shake_decay = shake_decay_start;
+		shake_intensity = shake_intensity_start;
+	}
+}
