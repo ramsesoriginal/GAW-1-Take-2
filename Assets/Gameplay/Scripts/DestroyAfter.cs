587aac00ecba61fb81d4308ebf1e25b1c7e897b8
Stefan Insam <ramsesoriginal@gmail.com>
2014-11-02 19:30:02 +0100

    Still working on level 5

---
+++ Assets/Gameplay/Scripts/DestroyAfter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfter : MonoBehaviour {
+
+	public float time = 1;
+	
+	// Update is called once per frame
+	void OnCollisionEnter () {
+		Invoke ("SelfDestrcut", time);
+	}
+
+	public void SelfDestrcut() {
+		Destroy (gameObject);
+	}
+
+}
